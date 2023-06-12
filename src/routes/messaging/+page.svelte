2d5f26d2a8e3fd1ca421a8ed7cbb8ebe4e871f01
Jawad <jawad.azizi1398@gmail.com>
2023-06-12 19:28:31 +0430

    message page updated

--- src/routes/messaging/+page.svelte
+++ src/routes/messaging/+page.svelte
@@ -4,6 +4,8 @@
     import PeopleCard from "$lib/components/PeopleCard.svelte";
     import userIcon from '$lib/icon/user.svg'
     import background from '$lib/image/8.jpg'
+    import MessageList from "./MessageList.svelte";
+    import ChatHistory from "./ChatHistory.svelte";
 
 
 </script>
@@ -11,28 +13,16 @@
 
 <div class="mynetwork grid grid-cols-12">
     
-    <div class="col-span-8 pb-2 grid grid-cols-12">
+    <div class="col-span-8 pb-2 grid grid-cols-12 border-2 rounded-lg bg-white border-gray-300" style="border-width: 0.5px;">
         <div class="col-span-5">
-            <div>
-                Messages
-            </div>
-            <div>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-                <CardWithPhoto src = {userIcon} title = 'someone'></CardWithPhoto>
-            </div>
+            <MessageList>
+
+            </MessageList>
         </div>
         <div class="col-span-7">
-            <div>title</div>
-            <div>
-                chat history 
-            </div>
+            <ChatHistory>
 
-           
+            </ChatHistory>
         </div>
         
     </div>
---
+++ src/routes/messaging/MessageList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import CardWithPhoto from "$lib/components/CardWithPhoto.svelte";
+    import userIcon from '$lib/icon/user.svg'
+
+</script>
+
+<div class="message-list border-gray-300 "  >
+    <div class=" px-2">
+        Message
+    </div>
+    <div  class="overflow-y-auto">
+        <CardWithPhoto src ={userIcon} title ='Someone'></CardWithPhoto>
+        <CardWithPhoto src ={userIcon} title ='Someone else'></CardWithPhoto>
+        <CardWithPhoto src ={userIcon} title ='Someone '></CardWithPhoto>
+        <CardWithPhoto src ={userIcon} title ='Someone else'></CardWithPhoto>
+        <CardWithPhoto src ={userIcon} title ='Someone'></CardWithPhoto>
+    </div>
+
+</div>
