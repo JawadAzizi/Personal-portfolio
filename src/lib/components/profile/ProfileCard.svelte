c4db8ab56d1a063275116171ad79511def069c9b
Jawad <jawad.azizi1398@gmail.com>
2023-06-18 18:05:01 +0430

    profile edit

--- src/lib/components/profile/ProfileCard.svelte
+++ src/lib/components/profile/ProfileCard.svelte
@@ -1,16 +1,18 @@
 <script lang='ts'>
     import backPhoto from '../../image/8.jpg'
     import userIcon from '../../icon/user.svg'
+	import CardWithPhoto from '../CardWithPhoto.svelte';
+    import KabulUnversityLogo from '$lib/image/kabulUniversityLogo.jpg'
 
 </script>
 
-<div class="rounded-lg bg-white  m-2">
+<div class="rounded-lg bg-white  m-2 overflow-hidden">
     <div class="background-photo">
         <img src="{backPhoto}" alt="backgound" class="w-full h-48">
     </div>
     <div class="relative py-6">
         <!--  avatar -->
-        <div class="absolute rounded-full bg-white" style="position: absolute;top: -4rem ; left: 1rem">
+        <div class="absolute rounded-full bg-white overflow-hidden" style="position: absolute;top: -4rem ; left: 1rem">
             <img src="{userIcon}" alt="avatar" class=" rounded-lg w-32">
         </div>
     </div>
@@ -22,15 +24,15 @@
             
             <div>connection</div>
         </div>
-        <div class="p-3  col-span-3">
+        <div class="p-2 float-left  col-span-3">
             <!-- education -->
-            kabul university
+            <CardWithPhoto src='{KabulUnversityLogo}' title='Kabul University'>
+            </CardWithPhoto>
         </div>
-
     </div>
     <div class="p-5">
         <div>
-            friends
+            Followers
         </div>
         <div>
             <button class="btn-primary ">message</button>
